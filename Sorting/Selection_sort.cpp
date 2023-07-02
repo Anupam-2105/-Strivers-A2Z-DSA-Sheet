9bf2ab93334882589ed2f9bbf97ea7c9c0aab746
Anupam DWivedi <anupamdwivedi2270@gmail.com>
2023-07-02 14:17:17 +0530

    Selction sort

---
+++ Sorting/Selection_sort.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selectionSort(int arr[],int n)
+{
+    int mini, temp;
+    for (int i = 0; i < n - 1; i++)
+    {
+        mini = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[mini] > arr[j])
+            {
+                mini = j;
+            }
+        }
+        temp = arr[i];
+        arr[i] = arr[mini];
+        arr[mini] = temp;
+    }
+}   
+
+int main()
+{
+    int arr[]{5,1,6,2,3,7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    selectionSort(arr,n);
+    for(auto it:arr){
+        cout<<it<<endl;
+    }
+}
