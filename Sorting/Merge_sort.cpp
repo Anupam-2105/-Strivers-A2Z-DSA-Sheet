ac49b0dd360a7a3ce2c32a3eeff4e205627be8f5
Anupam DWivedi <anupamdwivedi2270@gmail.com>
2023-07-04 18:52:10 +0530

    Merge sort

---
+++ Sorting/Merge_sort.cpp
@@ -0,0 +1,71 @@
+// Works by dividing an array into smaller subarrays, sorting each subarray, and then merging the sorted subarrays back together to form the final sorted array.
+// O(nlogn)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(int arr[], int l, int m, int r)
+{
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int a[n1], b[n2];
+    for (int i = 0; i < n1; i++)
+    {
+        a[i] = arr[l + i];
+    }
+    for (int j = 0; j < n2; j++)
+    {
+        b[j] = arr[m + j + 1];
+    }
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2)
+    {
+        if (a[i] <= b[j])
+        {
+            arr[k] = a[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = b[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        arr[k] = a[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = b[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+int main()
+{
+    int arr[]{5, 1, 6, 2, 3, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    mergeSort(arr, 0, n);
+    for (auto it : arr)
+    {
+        cout << it << endl;
+    }
+}
