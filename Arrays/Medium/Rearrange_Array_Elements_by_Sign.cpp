1a4e669a103ad44812aaa828339ab5bd0a8cc032
Anupam Dwivedi <anupamdwivedi2270@gmail.com>
2023-07-17 23:48:17 +0530

    Rearrange Array Elements by Sign

---
+++ Arrays/Medium/Rearrange_Array_Elements_by_Sign.cpp
@@ -0,0 +1,20 @@
+vector<int> rearrangeArray(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<int> ans(n, 0);
+    int pos = 0, neg = 1;
+    for (int it : nums)
+    {
+        if (it > 0)
+        {
+            ans[pos] = it;
+            pos += 2;
+        }
+        else
+        {
+            ans[neg] = it;
+            neg += 2;
+        }
+    }
+    return ans;
+}
