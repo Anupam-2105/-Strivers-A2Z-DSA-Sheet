6c37d7a64cd39761eb0b8206a395aa84d529e0d4
Anupam Dwivedi <anupamdwivedi2270@gmail.com>
2023-07-17 23:56:12 +0530

    Best Time to Buy and Sell Stock

---
+++ Arrays/Medium/Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,11 @@
+int maxProfit(vector<int> &prices)
+{
+    int mini = prices[0];
+    int ans = 0;
+    for (int i = 0; i < prices.size(); i++)
+    {
+        ans = max(ans, (prices[i] - mini));
+        mini = min(mini, prices[i]);
+    }
+    return ans;
+}
